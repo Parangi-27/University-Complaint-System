c999877a64213eed84fab882f68f38b7faf8e71a
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2023-03-09 23:33:34 +0530

    Authorization 1.0

---
+++ frontend/src/Component/ProfileCreate.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function ProfileCreate() {
+
+
+    // const [data, setData] = useState({
+    //     ui
+    // })
+
+  return (
+    <div>ProfileCreate</div>
+  )
+}
+
+export default ProfileCreate
